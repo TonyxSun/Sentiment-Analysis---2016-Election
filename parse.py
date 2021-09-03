53636f631a9bbf93579401f11234cf11d265c02f
Tony Sun <sunwuyue@live.com>
2021-09-03 12:52:16 -0700

    Define parse and stop word removal functions

---
+++ parse.py
@@ -0,0 +1,9 @@
+def clean_data(tw):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    cleaned_tw = ""
+    for char in tw:
+        if char not in punctuations:
+            cleaned_tw += char
+    return cleaned_tw
+
+print(clean_data("living the dream.#tommulcair instagram.com/p/8up9qepkxw/"))
---
+++ stop_words.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+def remove_stop_words(tw):
+    with open("stop_words.txt", 'r') as stop_words_txt:
+        stop_words = stop_words_txt.readlines()
+        stop_words_list = [word.strip() for word in stop_words]
+    tw_words = tw.split()
+    tw_stripped = ""
+    for word in tw_words:
+        if not word.lower() in stop_words_list:
+            tw_stripped += word
+        else:
+            print(word)
+    return tw_stripped
+        
+remove_stop_words("“living the dream.#tommulcair instagram.com/p/8up9qepkxw/")
