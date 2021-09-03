c2d04bad1a8db84d6b3a68598699ae74603cddac
Tony Sun <sunwuyue@live.com>
2021-09-03 13:21:05 -0700

    Add bag of words fucntion

---
+++ BofW.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+def clean_data(tw):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    cleaned_tw = ""
+    for char in tw:
+        if char == "#":
+            cleaned_tw += " "
+        elif char not in punctuations:
+            cleaned_tw += char
+    return cleaned_tw
+
+def remove_stop_words(tw):
+    with open("./Assets/stop_words.txt", 'r') as stop_words_txt:
+        stop_words = stop_words_txt.readlines()
+        stop_words_list = [word.strip() for word in stop_words]
+    tw_words = clean_data(tw).split()
+    tw_stripped = ""
+    for word in tw_words:
+        if not word.lower() in stop_words_list:
+            tw_stripped += word + " "
+        # else:
+        #     print(word)
+    return tw_stripped
+
+def tokenize_unigram(tw):
+    cleaned_tw = remove_stop_words(tw)
+    print(cleaned_tw)
+    return cleaned_tw.split()
+
+def bag_of_words(tw):
+    cleaned_tw = tokenize_unigram(tw)
+    bag_dict = {}
+    for token in cleaned_tw:
+        if token in bag_dict:
+            bag_dict[token] += 1
+        else:
+            bag_dict[token] = 1
+
+    return bag_dict
+
+print(bag_of_words("living the dream.#tommulcair  instagram.com/p/8up9qepkxw/"))
